0b1138e827aebced3f37118229c4b2ad0131172a
NSSimpleApps <ns.poluyanov.sergey@gmail.com>
2014-11-16 20:41:11 +0600

    Initial Commit

---
+++ CirclesOnScreen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CirclesOnScreen
+//
+//  Created by neo on 16.11.14.
+//  Copyright (c) 2014 NSSimpleApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CirclesOnScreen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CirclesOnScreen
+//
+//  Created by neo on 16.11.14.
+//  Copyright (c) 2014 NSSimpleApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
