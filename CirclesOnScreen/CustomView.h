3e39afb2fdeda1b9de0dd855854a632f6825839b
NSSimpleApps <ns.poluyanov.sergey@gmail.com>
2014-11-16 22:18:25 +0600

    First commit.

---
+++ CirclesOnScreen/CustomView.h
@@ -0,0 +1,13 @@
+//
+//  CustomView.h
+//  CirclesOnScreen
+//
+//  Created by neo on 16.11.14.
+//  Copyright (c) 2014 NSSimpleApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomView : UIView
+
+@end
